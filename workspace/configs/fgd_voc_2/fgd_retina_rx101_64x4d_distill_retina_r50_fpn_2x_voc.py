51e127662c8ba6b76ed0ad2f24ac919fe813970b
jwliang <961055871@qq.com>
2022-07-19 12:50:11 +0800

    add voc 2 card

--- workspace/configs/fgd_voc_2/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc.py
+++ workspace/configs/fgd_voc_2/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc.py
@@ -97,8 +97,8 @@ student_cfg = 'configs/retinanet_voc/retinanet_r50_fpn_2x_voc.py'
 teacher_cfg = 'configs/retinanet_voc/retinanet_x101_64x4d_fpn_1x_voc.py'
 
 # ===================
-batch_size = 4
-optimizer = dict( lr=0.01 / (16/batch_size))
+batch_size = 2
+optimizer = dict( lr=0.01 / (8/batch_size))
 optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
 data = dict(
     samples_per_gpu=batch_size,
--- workspace/configs/mgd_voc_2/mgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc.py
+++ workspace/configs/mgd_voc_2/mgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc.py
@@ -1,91 +1,71 @@
 _base_ = [
-    '../_base_/datasets/voc07_cocofmt.py',
-    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+    '../retinanet_voc/retinanet_r50_fpn_2x_voc.py'
 ]
 # model settings
 find_unused_parameters=True
-temp=0.5
-alpha_fgd=0.001
-beta_fgd=0.0005
-gamma_fgd=0.0005
-lambda_fgd=0.000005
-model = dict()
+alpha_mgd=0.00002
+lambda_mgd=0.65
 distiller = dict(
-    type='FGDDistiller',
+    type='MGDDistiller',
     teacher_pretrained = 'checkpoints/retinanet_x101_voc_24.pth',
     init_student = True,
     distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
                          teacher_module = 'neck.fpn_convs.4.conv',
                          output_hook = True,
-                         methods=[dict(type='FGDLoss',
-                                       name='loss_fgd_fpn_4',
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_4',
                                        student_channels = 256,
                                        teacher_channels = 256,
-                                       temp = temp,
-                                       alpha_fgd=alpha_fgd,
-                                       beta_fgd=beta_fgd,
-                                       gamma_fgd=gamma_fgd,
-                                       lambda_fgd=lambda_fgd,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
                                        )
                                 ]
                         ),
                     dict(student_module = 'neck.fpn_convs.3.conv',
                          teacher_module = 'neck.fpn_convs.3.conv',
                          output_hook = True,
-                         methods=[dict(type='FGDLoss',
-                                       name='loss_fgd_fpn_3',
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_3',
                                        student_channels = 256,
                                        teacher_channels = 256,
-                                       temp = temp,
-                                       alpha_fgd=alpha_fgd,
-                                       beta_fgd=beta_fgd,
-                                       gamma_fgd=gamma_fgd,
-                                       lambda_fgd=lambda_fgd,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
                                        )
                                 ]
                         ),
                     dict(student_module = 'neck.fpn_convs.2.conv',
                          teacher_module = 'neck.fpn_convs.2.conv',
                          output_hook = True,
-                         methods=[dict(type='FGDLoss',
-                                       name='loss_fgd_fpn_2',
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_2',
                                        student_channels = 256,
                                        teacher_channels = 256,
-                                       temp = temp,
-                                       alpha_fgd=alpha_fgd,
-                                       beta_fgd=beta_fgd,
-                                       gamma_fgd=gamma_fgd,
-                                       lambda_fgd=lambda_fgd,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
                                        )
                                 ]
                         ),
                     dict(student_module = 'neck.fpn_convs.1.conv',
                          teacher_module = 'neck.fpn_convs.1.conv',
                          output_hook = True,
-                         methods=[dict(type='FGDLoss',
-                                       name='loss_fgd_fpn_1',
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_1',
                                        student_channels = 256,
                                        teacher_channels = 256,
-                                       temp = temp,
-                                       alpha_fgd=alpha_fgd,
-                                       beta_fgd=beta_fgd,
-                                       gamma_fgd=gamma_fgd,
-                                       lambda_fgd=lambda_fgd,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
                                        )
                                 ]
                         ),
                     dict(student_module = 'neck.fpn_convs.0.conv',
                          teacher_module = 'neck.fpn_convs.0.conv',
                          output_hook = True,
-                         methods=[dict(type='FGDLoss',
-                                       name='loss_fgd_fpn_0',
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_0',
                                        student_channels = 256,
                                        teacher_channels = 256,
-                                       temp = temp,
-                                       alpha_fgd=alpha_fgd,
-                                       beta_fgd=beta_fgd,
-                                       gamma_fgd=gamma_fgd,
-                                       lambda_fgd=lambda_fgd,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
                                        )
                                 ]
                         ),
@@ -97,8 +77,8 @@ student_cfg = 'configs/retinanet_voc/retinanet_r50_fpn_2x_voc.py'
 teacher_cfg = 'configs/retinanet_voc/retinanet_x101_64x4d_fpn_1x_voc.py'
 
 # ===================
-batch_size = 4
-optimizer = dict( lr=0.01 / (16/batch_size))
+batch_size = 2
+optimizer = dict( lr=0.01 / (8/batch_size))
 optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
 data = dict(
     samples_per_gpu=batch_size,
