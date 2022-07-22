a79a2dc31c9902859f9f35301e407d24957c87e4
jwliang <961055871@qq.com>
2022-07-22 09:21:38 +0800

    add 2 layer config

---
+++ workspace/configs/cwd_voc_2/cwd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_advt_2l_0.00002.py
@@ -0,0 +1,111 @@
+_base_ = [
+    '../_base_/datasets/voc07_cocofmt.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+weight=5.0
+tau=1.0
+alpha_adv=0.00002
+distiller = dict(
+    type='CWDDistiller',
+    teacher_pretrained = 'checkpoints/retinanet_x101_voc_24.pth',
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='CWDLoss',
+                                       name='loss_cw_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       tau = tau,
+                                       weight =weight,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                       layer_idx=3
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+
+                         methods=[dict(type='CWDLoss',
+                                       tau = tau,
+                                       name='loss_cw_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       weight =weight,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                       layer_idx=2
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+
+                         methods=[dict(type='CWDLoss',
+                                       tau = tau,
+                                       name='loss_cw_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       weight =weight,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+
+                         methods=[dict(type='CWDLoss',
+                                       tau = tau,
+                                       name='loss_cw_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       weight =weight,
+                                       )
+                                ]
+                        ),
+
+
+                   ]
+    )
+
+student_cfg = 'configs/retinanet_voc/retinanet_r50_fpn_2x_voc.py'
+teacher_cfg = 'configs/retinanet_voc/retinanet_x101_64x4d_fpn_1x_voc.py'
+
+# ===================
+batch_size = 2
+optimizer = dict( lr=0.01 / (8/batch_size))
+optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
+
+train_pipeline = [
+    dict(type='LoadImageFromFile',adv_img='/public/data/dataset/adv_voc/'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize',
+         mean=[123.675, 116.28, 103.53],
+         std=[58.395, 57.12, 57.375],
+         to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'adv','gt_bboxes', 'gt_labels']),
+]
+
+data = dict(
+    samples_per_gpu=batch_size,
+    train=dict(
+        pipeline=train_pipeline),
+)
+evaluation = dict(interval=24)
+checkpoint_config= dict(interval=1,max_keep_ckpts=1)
