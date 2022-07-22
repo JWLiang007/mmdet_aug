99ea05491cd090efa73f3f60f0bc70e246814df7
jwliang <961055871@qq.com>
2022-07-22 10:46:06 +0800

    update 2 layer config

---
+++ workspace/configs/fgd_voc/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_advt_3_4l_0.00001.py
@@ -0,0 +1,140 @@
+_base_ = [
+    '../_base_/datasets/voc07_cocofmt.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+temp=0.5
+alpha_fgd=0.001
+beta_fgd=0.0005
+gamma_fgd=0.0005
+lambda_fgd=0.000005
+alpha_adv=0.00001
+model = dict()
+distiller = dict(
+    type='FGDDistiller',
+    teacher_pretrained = 'checkpoints/retinanet_x101_voc_24.pth',
+    init_student = True,
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
+                         teacher_module = 'neck.fpn_convs.4.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                       layer_idx=4
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                        layer_idx=3
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+student_cfg = 'configs/retinanet_voc/retinanet_r50_fpn_2x_voc.py'
+teacher_cfg = 'configs/retinanet_voc/retinanet_x101_64x4d_fpn_1x_voc.py'
+
+# ===================
+batch_size = 4
+optimizer = dict( lr=0.01 / (16/batch_size))
+optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
+
+train_pipeline = [
+    dict(type='LoadImageFromFile',adv_img='/home/jwl/project/mmdetection_voc/workspace/out/'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize',
+         mean=[123.675, 116.28, 103.53],
+         std=[58.395, 57.12, 57.375],
+         to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'adv','gt_bboxes', 'gt_labels']),
+]
+
+
+data = dict(
+    samples_per_gpu=batch_size,
+    train=dict(
+        pipeline=train_pipeline),
+)
+evaluation = dict(interval=24)
+checkpoint_config= dict(interval=1,max_keep_ckpts=1)
