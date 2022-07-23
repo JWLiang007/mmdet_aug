0e6eeae1dbc860817746a34c77c5da88f126e15e
jwliang <961055871@qq.com>
2022-07-23 16:23:11 +0800

    add adpt expr

--- workspace/configs/cwd_voc_2/cwd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adp_0.1_noCrop.py
+++ workspace/configs/cwd_voc_2/cwd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adp_0.1_noCrop.py
@@ -4,12 +4,11 @@ _base_ = [
 
 
 train_pipeline = [
-    dict(type='LoadImageFromFile',adv_img='/public/data/dataset/adv_voc/'),
+    dict(type='LoadImageFromFile',adv_img='/public/data/dataset/adv_voc_5_3/'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='InstanceAug',prob=0.1),
     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.0),
-    dict(type='RandomCrop', crop_size=(0.8, 0.8), crop_type='relative_range',adaptive = True),
     dict(type='Normalize',
          mean=[123.675, 116.28, 103.53],
          std=[58.395, 57.12, 57.375],
