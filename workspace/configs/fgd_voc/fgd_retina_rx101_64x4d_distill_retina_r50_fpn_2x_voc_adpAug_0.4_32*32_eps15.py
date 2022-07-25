cd6c4d8122655a1ad12d5f3c599c2dbe952fe3f2
jwliang <961055871@qq.com>
2022-07-25 16:42:16 +0800

    add fgd ablation expr config

--- workspace/configs/fgd_voc/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adpAug_0.4_32*32_eps15.py
+++ workspace/configs/fgd_voc/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adpAug_0.4_32*32_eps15.py
@@ -3,12 +3,11 @@ _base_ = [
 ]
 
 train_pipeline = [
-    dict(type='LoadImageFromFile',adv_img='/home/jwl/project/mmdetection_voc/workspace/out/'),
+    dict(type='LoadImageFromFile',adv_img='data/adv_voc_15_5'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='InstanceAug',prob=0.5),
+    dict(type='InstanceAug',prob=0.4),
     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.0),
-    dict(type='RandomCrop', crop_size=(0.8, 0.8), crop_type='relative_range',adaptive = True),
     dict(type='Normalize',
          mean=[123.675, 116.28, 103.53],
          std=[58.395, 57.12, 57.375],
