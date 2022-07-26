b809a8feaf7d20fbedfca4db4c57f7c156cd552e
jwliang <961055871@qq.com>
2022-07-26 13:08:09 +0800

    add fkd & fgd adpCrop ablation & cwd with init

--- mmdet/distillation/distillers/__init__.py
+++ mmdet/distillation/distillers/__init__.py
@@ -1,6 +1,7 @@
 from .fgd_distiller import FGDDistiller
 from .cwd_distiller import CWDDistiller
 from .mgd_distiller import MGDDistiller
+from .fkd_distiller import FKDDistiller
 __all__ = [
-    'FGDDistiller','CWDDistiller','MGDDistiller'
+    'FGDDistiller','CWDDistiller','MGDDistiller','FKDDistiller'
 ]
