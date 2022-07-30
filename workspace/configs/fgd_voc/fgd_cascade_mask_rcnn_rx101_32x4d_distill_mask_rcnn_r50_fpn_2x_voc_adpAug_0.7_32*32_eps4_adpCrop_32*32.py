14c60d6f1c39bffe2c4b5b6d0cfd0f937c7dd72c
jwliang <961055871@qq.com>
2022-07-31 00:07:15 +0800

    add mask_rcnn fgd config

---
+++ workspace/configs/fgd_voc/fgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_2x_voc_adpAug_0.7_32*32_eps4_adpCrop_32*32.py
@@ -0,0 +1,25 @@
+_base_ = [
+    './fgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_2x_voc.py'
+]
+
+train_pipeline = [
+    dict(type='LoadImageFromFile',adv_img='data/adv_voc_4_5'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='InstanceAug',prob=0.7),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='RandomCrop', crop_size=(0.8, 0.8), crop_type='relative_range', adaptive=True, bbox_size=(32, 32)),
+    dict(type='Normalize',
+         mean=[123.675, 116.28, 103.53],
+         std=[58.395, 57.12, 57.375],
+         to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+
+data = dict(
+    train=dict(
+        pipeline=train_pipeline),
+)
