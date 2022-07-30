a503c7e92f4bedbf7bc4569b0b9086ddcb85b58a
jwliang <961055871@qq.com>
2022-07-30 12:41:46 +0800

    add mask config

---
+++ workspace/configs/mask_rcnn_voc/mask_rcnn_r50_fpn_2x_voc_wto_aug.py
@@ -0,0 +1,24 @@
+_base_ = [
+    './mask_rcnn_r50_fpn_2x_voc.py',
+]
+
+
+# ======================
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+
+data = dict(
+    train=dict(
+        pipeline=train_pipeline),
+)
