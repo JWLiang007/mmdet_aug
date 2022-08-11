4e13298f3a81d78ad5277edcaefd588e0d35be0e
jwliang <961055871@qq.com>
2022-08-11 10:54:54 +0800

    add two stage config

---
+++ workspace/configs/fgd_voc_2/fgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_2x_voc.py
@@ -0,0 +1,88 @@
+_base_ = [
+    '../_base_/datasets/voc07_inst_cocofmt.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+temp=0.5
+alpha_fgd=0.00005
+beta_fgd=0.000025
+gamma_fgd=0.00005
+lambda_fgd=0.0000005
+distiller = dict(
+    type='FGDDistiller',
+    teacher_pretrained = 'checkpoints/cascade_mask_rcnn_x101_12.pth',
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                   ]
+    )
+
+student_cfg = 'configs/mask_rcnn_voc/mask_rcnn_r50_fpn_2x_voc.py'
+teacher_cfg = 'configs/dcn_voc/cascade_mask_rcnn_x101_32x4d_fpn_dconv_c3-c5_1x_voc.py'
+
+# =====================
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+auto_scale_lr = dict(enable=True)
+batch_size = 2
+data = dict(
+    samples_per_gpu=batch_size,
+)
