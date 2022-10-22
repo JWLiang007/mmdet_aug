e3c738d46879ce3a4aee8cd78928d3b936ef5b33
jwliang <961055871@qq.com>
2022-10-22 08:08:59 +0800

    rebuttal

---
+++ workspace/configs/mgd/mgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_coco_cutout.py
@@ -0,0 +1,96 @@
+_base_ = [
+    '../retinanet/retinanet_r50_fpn_2x_coco.py'
+]
+# model settings
+find_unused_parameters=True
+alpha_mgd=0.00002
+lambda_mgd=0.65
+distiller = dict(
+    type='MGDDistiller',
+    teacher_pretrained = 'checkpoints/retinanet_x101_64x4d_fpn_1x_coco_20200130-366f5af1.pth',
+    init_student = True,
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
+                         teacher_module = 'neck.fpn_convs.4.conv',
+                         output_hook = True,
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='MGDLoss',
+                                       name='loss_mgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_mgd=alpha_mgd,
+                                       lambda_mgd=lambda_mgd,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='CutOut',n_holes=2,cutout_ratio=(0.1,0.1)),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+student_cfg = 'configs/retinanet/retinanet_r50_fpn_2x_coco.py'
+teacher_cfg = 'configs/retinanet/retinanet_x101_64x4d_fpn_1x_coco.py'
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+data = dict(
+    train=dict(
+        pipeline=train_pipeline),
+)
