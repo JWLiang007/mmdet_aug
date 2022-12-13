c0c662b562b119b69197cfcede1411f60472eb7d
jwl <961055871@qq.com>
2022-12-13 16:59:51 +0800

    add config

--- workspace/configs/fgd_voc/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adpAug_0.3_full_stg1_eps8_adpCrop_32*32_advt_mse_0.00001_0_4l_eps8.py
+++ workspace/configs/fgd_voc/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc_adpAug_0.3_full_stg1_eps8_adpCrop_32*32_advt_mse_0.00001_0_4l_eps8.py
@@ -1,7 +1,8 @@
 _base_ = [
-    '../_base_/datasets/voc07_cocofmt.py',
-    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+    './fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_voc.py'
 ]
+
+
 # model settings
 find_unused_parameters=True
 temp=0.5
@@ -9,7 +10,10 @@ alpha_fgd=0.001
 beta_fgd=0.0005
 gamma_fgd=0.0005
 lambda_fgd=0.000005
-model = dict()
+# adv loss settings
+alpha_adv=0.00001
+loss_type='mse'
+
 distiller = dict(
     type='FGDDistiller',
     teacher_pretrained = 'checkpoints/retinanet_x101_voc_24.pth',
@@ -26,6 +30,14 @@ distiller = dict(
                                        beta_fgd=beta_fgd,
                                        gamma_fgd=gamma_fgd,
                                        lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                       layer_idx=4,
+                                        loss_type = loss_type,
                                        )
                                 ]
                         ),
@@ -41,6 +53,14 @@ distiller = dict(
                                        beta_fgd=beta_fgd,
                                        gamma_fgd=gamma_fgd,
                                        lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                        layer_idx=3,
+                                        loss_type = loss_type,
                                        )
                                 ]
                         ),
@@ -56,6 +76,14 @@ distiller = dict(
                                        beta_fgd=beta_fgd,
                                        gamma_fgd=gamma_fgd,
                                        lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                        layer_idx=2,
+                                        loss_type = loss_type,
                                        )
                                 ]
                         ),
@@ -71,6 +99,14 @@ distiller = dict(
                                        beta_fgd=beta_fgd,
                                        gamma_fgd=gamma_fgd,
                                        lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                        layer_idx=1,
+                                        loss_type = loss_type,
                                        )
                                 ]
                         ),
@@ -86,6 +122,14 @@ distiller = dict(
                                        beta_fgd=beta_fgd,
                                        gamma_fgd=gamma_fgd,
                                        lambda_fgd=lambda_fgd,
+                                       ),
+                                dict(type='AdvFeatureLoss',
+                                       name='adv_loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_adv=alpha_adv,
+                                        layer_idx=0,
+                                        loss_type = loss_type,
                                        )
                                 ]
                         ),
@@ -93,14 +137,26 @@ distiller = dict(
                    ]
     )
 
-student_cfg = 'configs/retinanet_voc/retinanet_r50_fpn_2x_voc.py'
-teacher_cfg = 'configs/retinanet_voc/retinanet_x101_64x4d_fpn_1x_voc.py'
 
-# ===================
-batch_size = 4
-optimizer = dict( lr=0.01 )
-auto_scale_lr = dict(enable=True)
-optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
+
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile',adv_img='data/adv_voc_8_5/'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='InstanceAug',prob=0.3,subst_full=True,subst_stg='1'),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='RandomCrop', crop_size=(0.8, 0.8), crop_type='relative_range', adaptive=True, bbox_size=(32, 32),subst_stg='1'),
+
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'adv','gt_bboxes', 'gt_labels']),
+]
+
 data = dict(
-    samples_per_gpu=batch_size,
+    train=dict(
+        pipeline=train_pipeline),
 )
