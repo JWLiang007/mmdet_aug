cd321b980321748f1b28e0337e5f449e4b1b0338
jwl <961055871@qq.com>
2023-01-14 00:54:02 +0800

    add mgd config;

---
+++ workspace/configs/mgd_coco_20c/mgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,104 @@
+_base_ = [
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters = True
+alpha_mgd = 0.0000005
+lambda_mgd = 0.45
+
+mgd_param = dict(
+    type="MGDLoss",
+    # name="mgd_loss",
+    student_channels=256,
+    teacher_channels=256,
+    alpha_mgd=alpha_mgd,
+    lambda_mgd=lambda_mgd,
+)
+distiller = dict(
+    type='FGDDistiller',
+    teacher_pretrained='checkpoints/cascade_mask_rcnn_x101_coco_20c_12.pth',
+    distill_cfg=[
+        dict(
+            student_module="neck.fpn_convs.0.conv",
+            teacher_module="neck.fpn_convs.0.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_0",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.1.conv",
+            teacher_module="neck.fpn_convs.1.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_1",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.2.conv",
+            teacher_module="neck.fpn_convs.2.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_2",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.3.conv",
+            teacher_module="neck.fpn_convs.3.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_3",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+            ],
+        ),
+    ])
+
+student_cfg = 'configs/mask_rcnn_coco_20c/mask_rcnn_r50_fpn_1x_coco.py'
+teacher_cfg = 'configs/dcn_coco_20c/cascade_mask_rcnn_x101_32x4d_fpn_dconv_c3-c5_1x_coco.py'
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+auto_scale_lr = dict(enable=True)
+
+CLASSES = ('bicycle', 'train', 'fire hydrant', 'stop sign', 'dog', 'bear',
+           'giraffe', 'snowboard', 'baseball bat', 'bottle', 'sandwich',
+           'broccoli', 'carrot', 'remote', 'cell phone', 'microwave',
+           'toaster', 'sink', 'clock', 'vase')
+data_root = 'data/coco/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        ann_file=data_root + 'annotations/instances_train2017_20c.json',
+        img_prefix=data_root + 'train2017/',
+        classes=CLASSES),
+    val=dict(
+        ann_file=data_root + 'annotations/instances_val2017_20c.json',
+        img_prefix=data_root + 'val2017/',
+        classes=CLASSES),
+    test=dict(
+        ann_file=data_root + 'annotations/instances_val2017_20c.json',
+        img_prefix=data_root + 'val2017/',
+        classes=CLASSES))
+
+custom_hooks = [dict(type='NumClassCheckHook'), dict(type='SetRunModeHook')]
---
+++ workspace/configs/mgd_coco_20c/mgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_1x_coco_adpAug_0.3_full_stg2_eps8_adpCrop_32*32_advt_mse_0.00000025_0_4l_eps8_advl_tea_0.5_0.125.py
@@ -0,0 +1,167 @@
+_base_ = [
+    './mgd_cascade_mask_rcnn_rx101_32x4d_distill_mask_rcnn_r50_fpn_1x_coco.py'
+]
+# model settings
+find_unused_parameters = True
+alpha_mgd = 0.0000005
+lambda_mgd = 0.45
+# adv loss settings
+alpha_adv = 0.00000025
+loss_type = 'mse'
+# dkd loss settings
+alpha_dkd = 0.5
+beta_dkd = 0.125
+temp_dkd = 1.0
+
+mgd_param = dict(
+    type="MGDLoss",
+    # name="mgd_loss",
+    student_channels=256,
+    teacher_channels=256,
+    alpha_mgd=alpha_mgd,
+    lambda_mgd=lambda_mgd,
+)
+
+adv_feat_param = dict(
+    type="AdvFeatureLoss",
+    # name="adv_loss",
+    student_channels=256,
+    teacher_channels=256,
+    alpha_adv=alpha_adv,
+    loss_type=loss_type,
+)
+
+adv_dkd_param = dict(
+    type="DKDLoss",
+    # name="dkd_loss",
+    alpha=alpha_dkd,
+    beta=beta_dkd,
+    temp=temp_dkd,
+)
+
+distiller = dict(
+    # type="FGDDistiller",
+    # teacher_pretrained="checkpoints/retinanet_x101_voc_24.pth",
+    # init_student=True,
+    distill_cfg=[
+        dict(
+            student_module="roi_head.bbox_head.loss_cls",
+            teacher_module="roi_head.bbox_head.2.loss_cls",
+            methods=[
+                dict(
+                    name="adv_dkd_loss",
+                    loss_input_type="logit",
+                    hook_type='input',
+                    logit_filter="teacher",
+                    threshold_p=0.0,
+                    img_type='adv',
+                    loss_param=adv_dkd_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.0.conv",
+            teacher_module="neck.fpn_convs.0.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_0",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+                dict(
+                    name="adv_loss_fpn_0",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='adv',
+                    loss_param=adv_feat_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.1.conv",
+            teacher_module="neck.fpn_convs.1.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_1",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+                dict(
+                    name="adv_loss_fpn_1",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='adv',
+                    loss_param=adv_feat_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.2.conv",
+            teacher_module="neck.fpn_convs.2.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_2",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+                dict(
+                    name="adv_loss_fpn_2",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='adv',
+                    loss_param=adv_feat_param,
+                ),
+            ],
+        ),
+        dict(
+            student_module="neck.fpn_convs.3.conv",
+            teacher_module="neck.fpn_convs.3.conv",
+            methods=[
+                dict(
+                    name="mgd_loss_fpn_3",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='clean',
+                    loss_param=mgd_param,
+                ),
+                dict(
+                    name="adv_loss_fpn_3",
+                    loss_input_type="feature",
+                    hook_type='output',
+                    img_type='adv',
+                    loss_param=adv_feat_param,
+                ),
+            ],
+        ),
+    ], )
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', adv_img='data/cmr_adv_coco_20c_8_5/'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='InstanceAug', prob=0.3, subst_full=True, subst_stg='1'),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(
+        type='RandomCrop',
+        crop_size=(0.8, 0.8),
+        crop_type='relative_range',
+        adaptive=True,
+        bbox_size=(32, 32),
+        subst_stg='1'),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(
+        type='Collect',
+        keys=['img', 'adv', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+
+data = dict(train=dict(pipeline=train_pipeline), )
