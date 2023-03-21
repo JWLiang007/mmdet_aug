13805acd10ea6f6c373968122b4064112189c09e
jwl <961055871@qq.com>
2023-03-21 23:37:15 +0800

    add copy paste

---
+++ configs/fgd/fgd_retina_rx101_64x4d_distill_retina_r50_fpn_2x_coco_copy_paste.py
@@ -0,0 +1,147 @@
+_base_ = [
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+temp=0.5
+alpha_fgd=0.001
+beta_fgd=0.0005
+gamma_fgd=0.0005
+lambda_fgd=0.000005
+distiller = dict(
+    type='FGDDistiller',
+    teacher_pretrained = 'checkpoints/retinanet_x101_64x4d_fpn_1x_coco_20200130-366f5af1.pth',
+    init_student = True,
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
+                         teacher_module = 'neck.fpn_convs.4.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fgd=gamma_fgd,
+                                       lambda_fgd=lambda_fgd,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+student_cfg = 'configs/retinanet/retinanet_r50_fpn_2x_coco.py'
+teacher_cfg = 'configs/retinanet/retinanet_x101_64x4d_fpn_1x_coco.py'
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+image_size = (1024, 1024)
+
+file_client_args = dict(backend='disk')
+
+# Standard Scale Jittering (SSJ) resizes and crops an image
+# with a resize range of 0.8 to 1.25 of the original image size.
+load_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='Resize',
+        img_scale=image_size,
+        ratio_range=(0.8, 1.25),
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(
+        type='RandomCrop',
+        crop_type='absolute_range',
+        crop_size=image_size,
+        recompute_bbox=True,
+        allow_negative_crop=True),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Pad', size=image_size),
+]
+train_pipeline = [
+    dict(type='CopyPaste', max_num_pasted=100),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        _delete_=True,
+        type='MultiImageMixDataset',
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + 'annotations/instances_train2017.json',
+            img_prefix=data_root + 'train2017/',
+            pipeline=load_pipeline),
+        pipeline=train_pipeline),)
