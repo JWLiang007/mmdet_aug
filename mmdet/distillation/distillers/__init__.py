8e475708c2bc21726612f1e92ab67adcf14684f0
jwl <961055871@qq.com>
2023-04-22 06:11:43 -0400

    add cwd to registry

--- mmdet/distillation/distillers/__init__.py
+++ mmdet/distillation/distillers/__init__.py
@@ -2,7 +2,8 @@ from .fgd_distiller import FGDDistiller
 
 from .mgd_distiller import MGDDistiller
 from .fkd_distiller import FKDDistiller
+from .cwd_distiller import CWDDistiller
 
 __all__ = [
-    'FGDDistiller','MGDDistiller','FKDDistiller'
+    'FGDDistiller','MGDDistiller','FKDDistiller', 'CWDDistiller'
 ]
--- mmdet/distillation/losses/__init__.py
+++ mmdet/distillation/losses/__init__.py
@@ -3,6 +3,7 @@ from .fgd_loss import  FGDLoss
 from .mgd_loss import MGDLoss
 from .adv_feature_loss import AdvFeatureLoss
 from .fkd_loss import FKDLoss
+from .cwd_loss import ChannelWiseDivergence
 __all__ = [
-    'FGDLoss' , 'MGDLoss','AdvFeatureLoss','FKDLoss'
+    'FGDLoss' , 'MGDLoss','AdvFeatureLoss','FKDLoss', 'ChannelWiseDivergence'
 ]
